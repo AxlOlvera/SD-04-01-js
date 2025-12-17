bc41800798ba6d9aec76577a8431ca2d63a5efa1
AxlOlvera <axl.sanchezolvera@gmail.com>
2025-12-17 16:45:42 -0600

    Se resolvieron ejercicios 6 y 7. Todos los ejercicios han sido resueltos

--- src/task7.js
+++ src/task7.js
@@ -6,6 +6,24 @@ const arr = [
   
   // Type your code below this line!
   
+  //Se agreg un sólo número a una fila existente
+  arr[0].splice(2,0,1); 
+  console.log(arr);
+
+  //Se agrega una fila completa de números
+  arr.splice(0,0, []);
+  for(let i = -1; i >= -10; i--){
+    arr[0].splice(0,0,i);
+  }
+  console.log(arr);
   
-  
+
+  //Se elimina un sólo número de una fila específica
+  arr[1].splice(2,1);
+  console.log(arr);
+
+  //Se invierte una de las filas sin afectar a las demás
+  arr[0].reverse();
+  console.log(arr);
+
   // Type your code above this line!
